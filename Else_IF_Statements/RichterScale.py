ad24c8749337985d52f5023b2db7e87496cabd01
NotTheSame1 <nseym07@yahoo.com>
2023-12-04 12:39:22 +1300

    Created program description and info

---
+++ Else_IF_Statements/RichterScale.py
@@ -0,0 +1,19 @@
+# RichterScale.py
+# @ Author: Norma Seym
+# Date: 01, December 2023
+"""
+Description:
+A program that reads a magnitude from the user and displays the appropriate
+descriptor.
+
+INFO: Magnitude Descriptor
+Less than 2.0 Micro
+2.0 - 2.9 Very minor
+3.0 - 3.9 Minor
+4.0 - 4.9 Light
+5.0 - 5.9 Moderate
+6.0 - 6.9 Strong
+7.0 - 7.9 Major
+8.0 - 9.9 Great
+10.0 or more Meteoric
+"""
