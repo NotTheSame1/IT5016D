3c305095cc8298a3eaef13c39a1203d294786f1f
NotTheSame1 <nseym07@yahoo.com>
2023-12-15 00:57:43 +1300

    Created module

---
+++ Lists/Slice_Change/Activity2_Challenge2.py
@@ -0,0 +1,16 @@
+# Activity2_Challenge2.py
+# @ Author: Norma Seym
+# Date: December 2023
+"""
+Info: A known Maori proverb translated to English states:
+'As man disappears from sight the land remains.'
+Parts of this proverb are written in the list shown.
+
+Directions:
+Write a program that requests input from the user, then inserts it into the list.
+Afterward, the program must join all the strings then output them to the screen.
+Assume that the user types in the correct input.
+"""
+
+proverb_part_list = [ "As a man disappears ", " the ", "land remains." ]
+
