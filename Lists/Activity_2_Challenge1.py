813948f880271092afb5e5f11325cafc289c9334
NotTheSame1 <nseym07@yahoo.com>
2023-12-14 23:04:29 +1300

    Added comments and test case assertions

--- Lists/Activity_2_Challenge1.py
+++ Lists/Activity_2_Challenge1.py
@@ -7,15 +7,33 @@ the sum or the average of the combined lists. Assume that the user enters valid 
 """
 list_1 = [23, 66, 23, 12]
 list_2 = [1, 19, 4, 8]
+# Uses built-in sum() function to get the sum of the combined lists.
 sum_list = sum(list_1) + sum(list_2)
 
+# Displays both lists.
 print("List 1:", list_1, "\nList 2:", list_2)
 
+# Prompts the user to choose to see either the sum or the average of the lists and saves into `option`.
 option = input("Display the (S)um or (A)verage of the combined lists? ")
+
+# Checks if `option` is "S".
 if option.upper() == "S":
+    # Displays the sum of the combined lists.
     print(f"The sum of the items on the lists is {sum_list}")
 
+# If the option is not "S"
 else:
+    # Adds the length of the combined lists.
     len_list = len(list_1) + len(list_2)
+    # Calculated the average of the combined lists.
     avg_list = sum_list / len_list
+    # Displays the average of the combined lists.
     print(f"The average of the items on the list is {avg_list}")
+
+"""
+Test case assertion 1:
+`option` is "S", result is sum = 156
+
+Test case assertion 2:
+`option` is "A", result is average = 19.5
+"""
