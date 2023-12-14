20877d9776c8c4c05fd3b2c0b3217fc4565d64a2
NotTheSame1 <nseym07@yahoo.com>
2023-12-15 00:09:57 +1300

    Completed challenge

---
+++ Lists/Slice_Change/Activity1_Challenge1.py
@@ -0,0 +1,10 @@
+# Activity1_Challenge1.py
+# @ Author: Norma Seym
+# Date: December 2023
+"""
+Directions:
+Write a program that prints a slice showing the 3rd, 4th and 5th elements of this list:
+"""
+list_1 = [34, 123, 5, 77, 59, 2, 4]
+# Displays the 3rd to 5th item in the list
+print(list_1[2:5])
