d951806cf1bcf036fb4ed52ba71f81b730cbb2f7
NotTheSame1 <nseym07@yahoo.com>
2023-12-14 22:29:19 +1300

    Declared lists. Printed list to display to user. Declared option. Started If statement to get sum of lists

---
+++ Lists/Activity_2_Challenge1.py
@@ -0,0 +1,16 @@
+# Activity_15.py
+# @ Author: Norma Seym
+# Date: December 2023
+"""
+Directions: Write a program that displays the first 2 lists. The user must be prompted and given a choice to see either
+the sum or the average of the combined lists. Assume that the user enters valid integers only.
+"""
+list_1 = [23, 66, 23, 12]
+list_2 = [1, 19, 4, 8]
+
+print("List 1:", list_1, "\nList 2:", list_2)
+
+option = input("Display the (S)um or (A)verage of the combined lists? ")
+if option.upper() == "S":
+    sum_list = sum(list_1) + sum(list_2)
+    print(sum_list)
