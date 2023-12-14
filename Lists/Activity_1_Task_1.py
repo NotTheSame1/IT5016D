c885148ae0f1031e169aecdd5db74f4f26f44383
NotTheSame1 <nseym07@yahoo.com>
2023-12-14 21:24:22 +1300

    Task completed

---
+++ Lists/Activity_1_Task_1.py
@@ -0,0 +1,38 @@
+# Activity_15.py
+# @ Author: Norma Seym
+# Date: December 2023
+"""
+Directions:
+- Copy out the example using in the module "lists".
+- Change any 3 index variables including at least one negative index. Make sure that you write the expected code output
+  in the form of comments.
+- Run your code to ensure that it works as expected.
+"""
+
+my_list = ["p", "r", "o", "b", "e"]
+
+# Output: r
+print(my_list[1])
+
+# Output: b
+print(my_list[3])
+
+
+# Nested List
+n_list = ["Happy", [2, 0, 1, 6]]
+
+# Nested indexing
+# Output: y
+print(n_list[0][4])
+
+# Output: 1
+print(n_list[1][2])
+
+
+# using negative indexing
+
+# Output: r
+print(my_list[-4])
+
+# Output: b
+print(my_list[-2])
