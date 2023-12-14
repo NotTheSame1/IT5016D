3cbfa5960b3a0fc8d2dc1e4b2d36eccd5f93ca41
NotTheSame1 <nseym07@yahoo.com>
2023-12-14 23:52:54 +1300

    Added comments and test assertions

--- Lists/Activity_2_Challenge5.py
+++ Lists/Activity_2_Challenge5.py
@@ -7,6 +7,18 @@ Directions: Write a program that outputs every odd valued index item from list_1
 
 list_1 = [23, 66, 23, 12]
 
+# iterates through the list
 for x in list_1:
+    # Checks if list item is not an even number
     if x % 2 != 0:
-        print(x, end=",")
+        # Displays items in list that are of odd value
+        print(x, end=", ")
+
+"""
+Test case assertion 1:
+Items displayed should be 23, 23
+
+Test case assertion 2:
+Temporarily changed items in list_1 to `list_1 = [1, 5, 23, 12]`
+Items displayed should be 1, 5, 23,
+"""
