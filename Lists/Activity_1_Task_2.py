be90c471d906183694f9677e05be3e981b094660
NotTheSame1 <nseym07@yahoo.com>
2023-12-14 21:40:04 +1300

    Completed task

---
+++ Lists/Activity_1_Task_2.py
@@ -0,0 +1,19 @@
+# Activity_15.py
+# @ Author: Norma Seym
+# Date: December 2023
+"""
+Directions:
+- Create a list containing the days of the week in Maori.
+- Print the 2nd and 5th days of the week in the console.
+- Print the day of the week that is 3 days from the end of the week.
+"""
+maori_days = ["Rāhina", "Rātu", "Rāapa", "Rāpare", "Rāmere", "Rāhoroi", "Rātapu"]
+
+# Output: Rātu
+print(maori_days[1], end=",")
+
+# Output: Rāmere
+print(maori_days[4], end=",")
+
+# Output Rāmere
+print(maori_days[-3], end=".")
