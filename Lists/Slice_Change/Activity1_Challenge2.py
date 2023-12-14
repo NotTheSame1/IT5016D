2c9134dc763f0fb65c29c95ec2d14e47b7638f4b
NotTheSame1 <nseym07@yahoo.com>
2023-12-15 00:31:20 +1300

    Created list. Declared expression to get length of list.

---
+++ Lists/Slice_Change/Activity1_Challenge2.py
@@ -0,0 +1,14 @@
+# Activity1_Challenge2.py
+# @ Author: Norma Seym
+# Date: December 2023
+"""
+Directions:
+- Create a stored list with an even number of elements. The list must have no fewer than 6 elements.
+- Write a program that returns the first half of all the elements of any list containing an even number of elements.
+"""
+my_list = [1, 4, 8, 9, 22, 27]
+
+# Determine how many items are in the list
+list_length = len(my_list)
+print(list_length)
+# Determine which numbers are the first half of the list
