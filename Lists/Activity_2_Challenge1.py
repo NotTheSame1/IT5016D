a4105758982974ffee9e0e15fbc9c893a26f2509
NotTheSame1 <nseym07@yahoo.com>
2023-12-14 22:46:42 +1300

    Created else statement. Moved sum_list to global scope.

--- Lists/Activity_2_Challenge1.py
+++ Lists/Activity_2_Challenge1.py
@@ -7,10 +7,15 @@ the sum or the average of the combined lists. Assume that the user enters valid 
 """
 list_1 = [23, 66, 23, 12]
 list_2 = [1, 19, 4, 8]
+sum_list = sum(list_1) + sum(list_2)
 
 print("List 1:", list_1, "\nList 2:", list_2)
 
 option = input("Display the (S)um or (A)verage of the combined lists? ")
 if option.upper() == "S":
-    sum_list = sum(list_1) + sum(list_2)
     print(sum_list)
+
+else:
+    len_list = len(list_1) + len(list_2)
+    avg_list = sum_list / len_list
+    print(avg_list)
