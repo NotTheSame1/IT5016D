568eaf0a4f3d21ad67e0cb31d7e0ee1ca58c570c
NotTheSame1 <nseym07@yahoo.com>
2023-12-15 22:31:42 +1300

    Completed challenge

---
+++ Dictionaries/Activity2_Challenge1.py
@@ -0,0 +1,13 @@
+# Activity2.py
+# @ Author: Norma Seym
+# Date: December 2023
+# Write a program that adds a stored dictionary item to a stored dictionary.
+
+# Dictionary containing names of people and their birth month.
+birth_month = {"Lily": "February", "Rosie": "May", "Junior": "December"}
+
+# Adding a key value pair to the dictionary.
+birth_month["Juan"] = "March"
+
+# Displays the new dictionary items
+print(birth_month)
